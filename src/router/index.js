e34be47d06ae750503bed163106d65ea96b505ec
tanjing <276887892@qq.com>
2020-03-02 13:38:10 +0800

    静态

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'  // 因为mine文件夹下是index.js 他会自动查找,所以不需要写mine/index.js
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+  movieRouter,
+  cinemaRouter,
+  mineRouter,
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/*',
+    redirect: '/movie'
   }
 ]
 
